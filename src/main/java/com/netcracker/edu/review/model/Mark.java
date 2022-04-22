c90d4a136ac579ac3edb2622879730e02f606b0a
Shagy798 <cotsarev.alex@yandex.ru>
2022-04-22 14:15:44 +0400

    make it better

---
+++ src/main/java/com/netcracker/edu/review/model/Mark.java
@@ -0,0 +1,20 @@
+package com.netcracker.edu.review.model;
+
+public class Mark {
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    private int id;
+    private String value;
+
+    public Mark() {
+    }
+
+
+}
---
+++ src/main/java/com/netcracker/edu/review/model/Role.java
@@ -0,0 +1,17 @@
+package com.netcracker.edu.review.model;
+
+public class Role {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    private String role;
+
+}
